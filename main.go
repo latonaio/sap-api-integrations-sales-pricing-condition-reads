9a984d4e0e1d7fbef7c3911ad19d5dbdc40fc303
Yurino Oga <yurino.o@latona.io>
2022-01-27 22:13:18 +0900

    release

--- main.go
+++ main.go
@@ -4,13 +4,13 @@ import (
 	sap_api_caller "sap-api-integrations-sales-pricing-reads/SAP_API_Caller"
 	"sap-api-integrations-sales-pricing-reads/sap_api_input_reader"
 
-	"github.com/latonaio/golang-logging-library/logger"
+	"github.com/latonaio/golang-logging-library-for-sap/logger"
 )
 
 func main() {
 	l := logger.NewLogger()
 	fr := sap_api_input_reader.NewFileReader()
-	inoutSDC := fr.ReadSDC("./Inputs//SDC_Sales_Pricing_Material_DistChannel_sample.json")
+	inoutSDC := fr.ReadSDC("./Inputs//SDC_Sales_Pricing_Material_Sales_Organization_DistChannel_sample.json")
 	caller := sap_api_caller.NewSAPAPICaller(
 		"https://sandbox.api.sap.com/s4hanacloud/sap/opu/odata/sap/", l,
 	)
